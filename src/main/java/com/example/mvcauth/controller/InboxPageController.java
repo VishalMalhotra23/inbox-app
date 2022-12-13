9518938beff3a8625db581cb02329f4fab2b248f
Uday Yadav <yadav117uday@gmail.com>
2022-12-13 12:12:15 +0530

    import all classes

---
+++ src/main/java/com/example/mvcauth/controller/InboxPageController.java
@@ -0,0 +1,78 @@
+package com.example.mvcauth.controller;
+
+
+import com.datastax.oss.driver.api.core.uuid.Uuids;
+import com.example.mvcauth.entities.EmailsList;
+import com.example.mvcauth.entities.Folder;
+import com.example.mvcauth.repository.EmailsListRepository;
+import com.example.mvcauth.repository.FolderRepository;
+import com.example.mvcauth.service.FoldersService;
+import com.nimbusds.oauth2.sdk.util.StringUtils;
+import org.ocpsoft.prettytime.PrettyTime;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class InboxPageController {
+
+    private final FolderRepository folderRepository;
+    private final EmailsListRepository emailsListRepository;
+    private final FoldersService foldersService;
+
+    private final PrettyTime prettyTime;
+
+    @Autowired
+    public InboxPageController(FolderRepository folderRepository,
+                               EmailsListRepository emailsListRepository,
+                               FoldersService foldersService) {
+        this.folderRepository = folderRepository;
+        this.emailsListRepository = emailsListRepository;
+        this.foldersService = foldersService;
+        this.prettyTime = new PrettyTime();
+    }
+
+
+
+    @GetMapping(value = "/")
+    public String getHomePage(@RequestParam(required = false) String folder,
+                              @AuthenticationPrincipal OAuth2User principal, Model model) {
+        if (principal != null && principal.getAttribute("login") != null) {
+            String loginId = principal.getAttribute("login");
+            List<Folder> folders = folderRepository.findAllById(loginId);
+            List<Folder> initFolders = foldersService.init(loginId);
+            // initFolders.stream().forEach(folderRepository::save);
+            model.addAttribute("defaultFolders", initFolders);
+            if (folders.size() > 0) {
+                model.addAttribute("userFolders", folders);
+            }
+            if (StringUtils.isBlank(folder)) {
+                folder = "Inbox";
+            }
+            model.addAttribute("currentFolder", folder);
+            Map<String, Integer> folderToUnreadCounts = foldersService.getUnreadCountsMap(loginId);
+            model.addAttribute("folderToUnreadCounts", folderToUnreadCounts);
+            List<EmailsList> emails = emailsListRepository.findAllById_UserIdAndId_Label(loginId, folder);
+            emails.forEach(email -> {
+                Date emailDate = new Date(Uuids.unixTimestamp(email.getId().getTimeId()));
+                email.setAgoTimeString(prettyTime.format(emailDate));
+            });
+            model.addAttribute("folderEmails", emails);
+
+            return "inbox-page";
+        }
+        return "index";
+
+    }
+
+
+
+}
---
+++ src/main/java/com/example/mvcauth/service/EmailService.java
@@ -0,0 +1,79 @@
+package com.example.mvcauth.service;
+import com.datastax.oss.driver.api.core.uuid.Uuids;
+import com.example.mvcauth.entities.Email;
+import com.example.mvcauth.entities.EmailsList;
+import com.example.mvcauth.entities.key.EmailsListPrimaryKey;
+import com.example.mvcauth.repository.EmailRepository;
+import com.example.mvcauth.repository.EmailsListRepository;
+import com.example.mvcauth.repository.UnreadEmailStatsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Service
+public class EmailService {
+
+    private final EmailsListRepository emailsListRepository;
+    private final EmailRepository emailRepository;
+    private final UnreadEmailStatsRepository unreadEmailStatsRepository;
+
+    @Autowired
+    public EmailService(EmailsListRepository emailsListRepository, EmailRepository emailRepository, UnreadEmailStatsRepository unreadEmailStatsRepository) {
+        this.emailsListRepository = emailsListRepository;
+        this.emailRepository = emailRepository;
+        this.unreadEmailStatsRepository = unreadEmailStatsRepository;
+    }
+
+    public void sendEmail(String fromUserId, String toUserIds, String subject, String body) {
+
+        UUID timeUuid = Uuids.timeBased();
+
+        List<String> toUserIdList = Arrays.asList(toUserIds.split(",")).stream()
+                .map(id -> StringUtils.trimWhitespace(id)).filter(id -> StringUtils.hasText(id))
+                .collect(Collectors.toList());
+        // Add to sent items of sender
+        EmailsList sentItemEntry = prepareEmailsListEntry("Sent", fromUserId, fromUserId, toUserIdList, subject,
+                timeUuid);
+        sentItemEntry.setRead(true);
+        emailsListRepository.save(sentItemEntry);
+        // Add to inbox of each reciever
+        toUserIdList.stream().forEach(toUserId -> {
+            EmailsList inboxEntry = prepareEmailsListEntry("Inbox", toUserId, fromUserId, toUserIdList, subject,
+                    timeUuid);
+            inboxEntry.setRead(false);
+            emailsListRepository.save(inboxEntry);
+            unreadEmailStatsRepository.incrementUnreadCounter(toUserId, "Inbox");
+        });
+
+        // Save email entity
+        Email email = new Email();
+        email.setId(timeUuid);
+        email.setFrom(fromUserId);
+        email.setTo(toUserIdList);
+        email.setSubject(subject);
+        email.setBody(body);
+        emailRepository.save(email);
+
+    }
+
+    private EmailsList prepareEmailsListEntry(String folderName, String forUser, String fromUserId,
+                                              List<String> toUserIds, String subject, UUID timeUuid) {
+
+        EmailsListPrimaryKey key = new EmailsListPrimaryKey();
+        key.setLabel(folderName);
+        key.setUserId(forUser);
+        key.setTimeId(timeUuid);
+        EmailsList emailsListEntry = new EmailsList();
+        emailsListEntry.setId(key);
+        emailsListEntry.setFrom(fromUserId);
+        emailsListEntry.setTo(toUserIds);
+        emailsListEntry.setSubject(subject);
+        return emailsListEntry;
+    }
+
+}
