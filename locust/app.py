73216c1f6f11ebd23851f93e744308d0347d1945
Uday Yadav <dev117uday@gmail.com>
2023-06-16 16:33:13 +0530

    update locust

--- locust/app.py
+++ locust/app.py
@@ -5,92 +5,65 @@ from gevent.pool import Pool
 import time
 
 fake = Faker()
- 
+
 class MySQTest(SequentialTaskSet):
 
-    firstuser = fake.email()
-    seconduser = fake.email()
-
     @task
-    def send_email_user(self):
+    def generateLoad(self):
 
-        def concurrent_request():
-            message = json.dumps({
-                "from": self.firstuser,
-                "to": self.seconduser,
-                "body": fake.sentence(),
-                "subject": fake.sentence()
-            })
+        firstuser = fake.email()
+        seconduser = fake.email()
 
-            self.client.post("/api/compose", data=str(message), headers = {'Content-Type': 'application/json'})
-            
-            message = json.dumps({
-                "from": self.seconduser,
-                "to": self.firstuser,
-                "body": fake.sentence(),
-                "subject": fake.sentence()
-            })
-
-            self.client.post("/api/compose", data=str(message), headers = {'Content-Type': 'application/json'})
-        
-        pool = Pool()
         for i in range(5):
-            pool.spawn(concurrent_request)
-        pool.join()
 
+            message = json.dumps({
+                "from": firstuser,
+                "to": seconduser,
+                "body": fake.sentence(),
+                "subject": fake.sentence()
+            })
 
+            self.client.post("/api/compose", data=str(message), headers = {'Content-Type': 'application/json'})
 
-    # @task
-    # def send_email_user2(self):
+            message = json.dumps({
+                "from": seconduser,
+                "to": firstuser,
+                "body": fake.sentence(),
+                "subject": fake.sentence()
+            })
 
-    #     def concurrent_request():
-    #         message = json.dumps({
-    #             "from": self.seconduser,
-    #             "to": self.firstuser,
-    #             "body": fake.sentence(),
-    #             "subject": fake.sentence()
-    #         })
+            self.client.post("/api/compose", data=str(message), headers = {'Content-Type': 'application/json'})
 
-    #         self.client.post("/api/compose", data=str(message), headers = {'Content-Type': 'application/json'})
-        
-    #     pool = Pool()
-    #     for i in range(5):
-    #         pool.spawn(concurrent_request)
-    #     pool.join()
-        
-
-    @task
-    def read_all_email_user1(self):
-        time.sleep(1) 
-
-        res = self.client.get(f"/api/email/{self.firstuser}/i")
+        time.sleep(1)
+               
+        res = self.client.get(f"/api/email/{firstuser}/i")
         data = json.loads(res.text)
 
-        def concurrent_request(i):        
+        time.sleep(1)
+
+        def concurrent_request1(i):
             x = data[i].get("id").get("timeId") 
             self.client.get(f"/api/email/{x}")
-        
+
         pool = Pool()
         for i in range(len(data)):
-            pool.spawn(concurrent_request, i)
+            pool.spawn(concurrent_request1,i)
         pool.join()
-
-    @task
-    def read_all_email_user2(self):
-        time.sleep(1) 
-        res = self.client.get(f"/api/email/{self.seconduser}/i")
+        
+        res = self.client.get(f"/api/email/{seconduser}/i")
         data = json.loads(res.text)
 
-        def concurrent_request(i):        
+        time.sleep(1)
+        def concurrent_request(i):
             x = data[i].get("id").get("timeId") 
             self.client.get(f"/api/email/{x}")
-        
+
         pool = Pool()
         for i in range(len(data)):
-            pool.spawn(concurrent_request, i)
+            pool.spawn(concurrent_request,i)
         pool.join()
-    
+
 class MyLoad(FastHttpUser):
 
-    host = "http://localhost:8080"
+    host = "http://10.128.0.24"
     tasks = [MySQTest]
